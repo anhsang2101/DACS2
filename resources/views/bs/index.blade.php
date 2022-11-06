c7bea452795316cbe9a008549f7079c481a007b6
nguyenmanhtruongdat <datnmt.21it@vku.udn.vn>
2022-11-06 20:57:40 +0700

    dat

--- resources/views/bs/index.blade.php
+++ resources/views/bs/index.blade.php
@@ -57,6 +57,36 @@
             <a class="btn btn-success" href="recruit">Tuyển dụng ngay</a>
         </div>
     </header>
+    <div class="page-wrapper chiller-theme ml-0 mt-3 bg-white">
+        <div class="container">
+            <div class="row" style="margin-right: -15px;margin-left: -15px;">
+                <div class="col-md-6">
+                    <div class="box-create-cv mb-16">
+                        <div class="col-md-7 box-item">
+                            <h2>Đăng tin tuyển dụng dễ dàng nhanh chóng </h2>
+                            <h3>Với EZCV bạn có thể dễ dàng đăng tin tuyển dụng mọi lúc mọi nơi</h3>
+                            <a href="create-new-job" class="btn btn-topcv btn-search-job" style="font-weight: bold;"><i class="fa-solid fa-plus"></i> Đăng tin ngay</a>
+                        </div>
+                        <div class="box-image">
+                            <img src="{{ url('img/dangtin1.png') }}" alt="Dang tin" title="Tạo CV online ấn tượng" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="box-create-cv">
+                        <div class="col-md-7 box-item">
+                            <h2>Quản lý tin tuyển dụng hiệu quả</label> </h2>
+                            <h3>Dễ dàng quản lý và thống kê tin đã đăng, cho phép hiệu quả và năng suất đến 60%</h3>
+                            <a href="manage-recruitment" class="btn btn-topcv btn-search-job" style="font-weight: bold;"> <i class="fa-solid fa-bars-progress"></i> Vào trang quản lý</a>
+                        </div>
+                        <div class="box-image box-upload">
+                            <img src="https://www.topcv.vn/v4/image/welcome/upload-cv.png?v=1.0.0" title="Sử dụng CV theo ngành nghề" alt="Su dung CV theo nganh nghe" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
